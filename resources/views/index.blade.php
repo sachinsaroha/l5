43ea9a78bed6c40dd75ba81ab9f8308262d4b584
Sumit Dabas <sumit@192-168-1-54.tpgi.com.au>
2016-04-07 00:45:44 +1000

    index blade file

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Index</title>
+
+        
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <h1>Hello World</h1>
+            </div>
+        </div>
+    </body>
+</html>
