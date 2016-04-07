ba261573d7ec875f380f0b901253734b0ea23e99
Sumit Dabas <sumit@sumitMac.local>
2016-04-07 20:18:04 +1000

    view added

---
+++ resources/views/displayDataView.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Index</title>
+
+        
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+
+                <h1>display Data</h1>
+
+                {{print_r($data)}}
+            </div>
+        </div>
+    </body>
+</html>
