b0ad453b83e16a3bd77f006f09cf1ac79ec7c47b
Sumit Dabas <sumit@sumitMac.local>
2016-04-10 11:16:46 +1000

    controller gets data from route and passes data from model to view

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,13 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
+use App\Customer;
 
 class AdminController extends Controller
 {
+	public $restful = true;
+    
     public function index(){
     	
     	return view('index');
@@ -16,5 +17,15 @@ class AdminController extends Controller
 
     public function getData(){
     	
+    	$dat=['hi', 'this', 'is', 'new', 'data1'];
+    	return view('displayDataView')->with('data',$dat);
+    }	
+
+    public function displayID($id){
+        
+        $customer = Customer::find($id);
+        //echo "Customer : ".$customer->name;
+        $dat = $customer->name;
+        return view('displayDataView')->with('data', $dat);
     }
 }
